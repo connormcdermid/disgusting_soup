c5dfc43e0c70929651096b6ac2fd7278a049eaa1
Connor McDermid <mcdermidconnor@outlook.com>
2025-03-24 14:39:15 -0700

    Added get data as string function
    Signed-off-by: Connor McDermid <mcdermidconnor@outlook.com>

--- netcode.py
+++ netcode.py
@@ -185,5 +185,21 @@ def get_data_as_file(dept: str, session: str = "S2025") -> None:
     filewriter(trimmed)
     return
 
+def get_data_as_string(dept: str, session: str = "S2025") -> str:
+    """
+    Get timetable data returned as a string.
+
+    Write timetable data returned as a string to be used by later programs.
+    Please pass this function: a course area, as a three- or four-letter department code,
+    and the term you wish to fetch, as a string in the format S<year> or F<year>,
+    defaulting to S2025.
+    :param dept: Deparment code or name
+    :param session: Term code
+    :return: String containing XML response
+    """
+    xml = makeRequest(dept, session)
+    trimmed = trim(xml)
+    return trimmed
+
 if __name__ == '__main__':
     get_data_as_file("CSCI")
