31ac40f2e5430b9b01a27f83dcda5e58bcc58ecc
Luka Karanovic <karanovl@stumail.csci.viu.ca>
2025-04-02 12:33:14 -0700

    algorithm class started

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,22 @@
+from Day import Day
+from timetable import makeTimetable
 
+
+
+class algorithm:
+    
+    def __init__(self, meetingLength: int, days: list[Day]):
+        
+        self.meetingLength = meetingLength / 30
+        
+    
+    def bestTimeInDay(self, day: Day) -> list:
+        
+
+
+        pass
+    
+    def bestTimeInWeek(self,):
+    
+if __name__ == '__main__':
+    a = algorithm(30)
