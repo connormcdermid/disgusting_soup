057e094506e376a8ab7108a30767ea817740214d
oohkmail <liamkhoo4@gmail.com>
2025-04-04 17:34:08 -0700

    algorithm
    worked on interfacing with rest of program

--- algorithm.py
+++ algorithm.py
@@ -1,81 +1,111 @@
 from Day import Day
-from timetable import makeTimetable
-from timetable import getDayFromIndex
+from timetable import makeTimetable, getDayFromIndex
+from timetable import *
 
-class algorithm:
-    
-    
-    
-    def addTime(self,a,b)->int:
-        """adds a number of minutes to a time, and returns the result
+def addTime(a, b) -> int:
+    """adds a number of minutes to a time, and returns the result
 
-        Args:
-            a (_type_): the time to start with. Example "700" is 7 am.
-            b (_type_): the number of Minutes to add to time a. Example "90" is one and a half hours.
+    Args:
+        a (_type_): the time to start with. Example "700" is 7 am.
+        b (_type_): the number of Minutes to add to time a. Example "90" is one and a half hours.
 
-        Returns:
-            int: the time 'a' 'b' minutes later.
-        """
-        time=a+(int(b/60)*100)
-        #print(time)
-        time=time+(b%60)
-        if(time>2400):
-            time=time-2400
-        if(time%100>59):
-            time=time+40
-            
-        return int(time)
+    Returns:
+        int: the time 'a' 'b' minutes later.
+    """
+    time = a + (int(b / 60) * 100)
+    #print(time)
+    time = time + (b % 60)
+    if(time > 2400):
+        time = time - 2400
+    if(time % 100 > 59):
+        time = time + 40
         
+    return int(time)
     
-    def bestTimeInDay(self,day:Day, meetingLength:int,dayName:str)->dict:
-        """Computes a "viability" score for each possible meeting time in a day.
 
-        Args:
-            day (Day): Contains course schedule for that day
-            meetingLength (int): length of the meeting in MINUTES.
-            dayName (str): the name of the day
+def calculateDayScores(day: Day, meetingLength: int, dayName: str) -> dict:
+    """Computes a "viability" score for each possible meeting time in a day.
 
-        Returns:
-            list: some sort of tuple with the time block start and end, and its "viability" score? 
-        """
-        meetingBlocks={}
-        for timeblock in day.table.keys(): #we start at each 30 minute timeblock in a day
-            score=0
-            start=int(timeblock[0])
-            if(self.addTime(start,meetingLength))<2100: #check if the time is early enough to have a full meeting before the day is over (9pm)
-                meetingStart=start
-                for i in range (1,int(meetingLength/30),1): #count the number of 30 min blocks the meeting takes, and check that number of blocks after the supposed start
-                    print("i is ",i)
-                    #sum number of classes in the meeting block
-                    print('start',str(meetingStart).zfill(4))
-                    print('end',str(self.addTime(meetingStart,i*30)).zfill(4))
-                    
-                    a=day.table[(str(meetingStart).zfill(4),str(self.addTime(meetingStart,30)).zfill(4))] #the list of courses occuring at a 30 min block
-                    score += len(a)
-                    meetingStart=self.addTime(meetingStart,30)
-                    #print(start)
-                    
-                        
-            meetingBlocks[(str(start).zfill(4),str(self.addTime(start,meetingLength)).zfill(4),dayName)]=score
-            
-        return meetingBlocks       
+    Args:
+        day (Day): Contains course schedule for that day
+        meetingLength (int): length of the meeting in MINUTES.
+        dayName (str): the name of the day
+
+    Returns:
+        list: some sort of tuple with the time block start and end, and its "viability" score? 
+    """
+    dayScores = {}
+    for timeblock in day.table.keys(): #we start at each 30 minute timeblock in a day
+        score = 0
+        start = int(timeblock[0])
+        if(addTime(start, meetingLength)) < 2100: #check if the time is early enough to have a full meeting before the day is over (9pm)
+            meetingStart = start
+            for i in range (1, int(meetingLength / 30), 1): #count the number of 30 min blocks the meeting takes, and check that number of blocks after the supposed start
+                #print("i is ",i)
+                #sum number of classes in the meeting block
+                #print('start',str(meetingStart).zfill(4))
+                #print('end',str(self.addTime(meetingStart,i*30)).zfill(4))
                 
+                a = day.table[(str(meetingStart).zfill(4), str(addTime(meetingStart, 30)).zfill(4))] #the list of courses occuring at a 30 min block
+                score += len(a)
+                meetingStart = addTime(meetingStart, 30)
+                #print(start)
+                
+                    
+        dayScores[(str(start).zfill(4), str(addTime(start, meetingLength)).zfill(4), dayName)] = score
+        
+    return dayScores      
             
-            
-    
-    def bestTimeInWeek(self,week:list[Day],meetingLength:int):
-        """For every day in the week, compute the  3 best meeting times.
+"""
+number of classes < 3
+number of classes in block before and after is much higher
+time is closer to the middle of the day
 
-        Args:
-            week (list[Day]): Represenation of all classes in a week
-            meetingLength (int): length of the meeting in mintues.
-        """
+timeBefore: 4
+time: 2
+timeAfter: 2
+time is at 1300
+score = 2 * 0 * 3 + 5 
+
+score = (num. class before - num. class) * (num. class after - num. class) * (5 - num. class) + timeBonus
+"""        
         
-        #in this function we will select the highest scores adn return them.
-        
-        
-        bestTimes = []
-        for i in range(0,7):
-            bestTimes.append(self.bestTimeInDay(week[i],meetingLength,getDayFromIndex(i)))
-        
+
+def bestTimesInWeek(week: list[Day], meetingLength: int):
+    """For every day in the week, compute the  3 best meeting times.
+
+    Args:
+        week (list[Day]): Represenation of all classes in a week
+        meetingLength (int): length of the meeting in mintues.
+    """
+    allTimes = {}
     
+    for day in week:
+        allTimes = allTimes | calculateDayScores(day, meetingLength, getDayFromIndex(week.index(day)))
+
+    sorted_by_values_desc = sorted(allTimes.items(), key = lambda item: item[1], reverse = True)
+
+    topScores = []
+    for i in range(5):
+        topScores.append(sorted_by_values_desc[i][0])
+
+    return topScores
+
+    #in this function we will select the highest scores adn return them.
+    
+    
+    
+    # print(sorted_by_values_desc)
+
+    bestTimes = []
+    for i in range(0, 7):
+        bestTimes.append(calculateDayScores(week[i], meetingLength, getDayFromIndex(i)))
+    
+
+if __name__ == '__main__':
+    soup = getSoup()
+    h = get_timetable(soup)
+    #print(h)
+
+
+    print(bestTimesInWeek(h, 90))
